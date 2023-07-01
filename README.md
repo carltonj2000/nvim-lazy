f3a45bd8d514c10d75cf943a7f00b3799ca485e8
Carlton Joseph <carlton.joseph@gmail.com>
2023-07-01 14:43:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# LazyVim Carlton Customization
+
+This config was created base on the following:
+
+- https://youtu.be/N93cTbtLCIM
+
+# 💤 LazyVim
+
+A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.winbar = "%=%m %f"
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+vim.opt.swapfile = false
